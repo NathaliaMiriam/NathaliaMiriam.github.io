3103b30ad86836e26554399077416a2d8f8793f4
NathaliaMiriam <nathalia.miriam@hotmail.com>
2023-02-06 20:30:44 -0300

    projects images + starts projects page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,74 +18,75 @@ function Projects() {
           Meus <strong className="purple">Projetos Recentes </strong>
         </h1>
         <p style={{ color: "white" }}>Em Construção...</p>
-        {/* <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={chatify}
-              isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
+        {
+          <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+            <Col md={4} className="project-card">
+              <ProjectCard
+                imgPath={chatify}
+                isBlog={false}
+                title="Node-MySQL-API"
+                description="API com Node.js e MySQL utilizando CRUD completo."
+                ghLink="https://github.com/NathaliaMiriam/Node-MySQL-API"
+                //demoLink="https://chatify-49.web.app/"
+              />
+            </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
-            />
-          </Col>
+            <Col md={4} className="project-card">
+              <ProjectCard
+                imgPath={bitsOfCode}
+                isBlog={false}
+                title="Pokedex-react"
+                description="Pokedex de página única utilizando React.js, MUI, Axios e a PokéAPI pública."
+                ghLink="https://github.com/NathaliaMiriam/Pokedex-react"
+                demoLink="https://nathaliamiriam.github.io/Pokedex-react/"
+              />
+            </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
-            />
-          </Col>
+            <Col md={4} className="project-card">
+              <ProjectCard
+                imgPath={editor}
+                isBlog={false}
+                title="Pokedex"
+                description="Pokedex responsiva feita com JavaScript assíncrono, HTML, CSS e a PokéAPI pública"
+                ghLink="https://github.com/NathaliaMiriam/Pokedex"
+                demoLink="https://nathaliamiriam.github.io/Pokedex/"
+              />
+            </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
+            <Col md={4} className="project-card">
+              <ProjectCard
+                imgPath={leaf}
+                isBlog={false}
+                title="Responsive-website"
+                description="Página responsiva feita com JavaScript, HTML e SCSS."
+                ghLink="https://github.com/NathaliaMiriam/Responsive-website"
+                demoLink="https://nathaliamiriam.github.io/Responsive-website/"
+              />
+            </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
+            <Col md={4} className="project-card">
+              <ProjectCard
+                imgPath={suicide}
+                isBlog={false}
+                title="Form"
+                description="Formulário simples e ao mesmo tempo elegante feito com HTML e CSS."
+                ghLink="https://github.com/NathaliaMiriam/Form"
+                demoLink="https://nathaliamiriam.github.io/Form/"
+              />
+            </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
-            />
-          </Col>
-        </Row> */}
+            <Col md={4} className="project-card">
+              <ProjectCard
+                imgPath={emotion}
+                isBlog={false}
+                title="NN-roupas-pet"
+                description="Projeto em HTML e CSS para um ateliê de roupas e acessórios customizados para Pets."
+                ghLink="https://github.com/NathaliaMiriam/NN-roupas-pet"
+                demoLink="https://nathaliamiriam.github.io/NN-roupas-pet/"
+              />
+            </Col>
+          </Row>
+        }
       </Container>
     </Container>
   );
